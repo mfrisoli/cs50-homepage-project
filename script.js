ee9766aafa42ab34b944da313ecce221a9bdf263
Marco Frisoli <marcof787@gmail.com>
2020-06-13 13:13:24 +0100

    calculator test

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function message()
     }
     alert(text_message);
 }
+
+// Calculator Script
+
+function dis(val)
+{
+    document.getElementById("result").value += val
+}
+
+function solve()
+{
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+function clr()
+{
+    document.getElementById("result").value = ""
+}
